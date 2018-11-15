873241a028e9a05cd1dfddc677330af1680c9f93
masato <masato@m-mbp.local>
2018-11-15 21:19:04 +0800

    Initial Commit

---
+++ UIProgressView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIProgressView
+//
+//  Created by masato on 15/11/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
